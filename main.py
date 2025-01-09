f763ef78d1476fafb2bc3509dae704b549cfa147
sam1037 <samngai2522@gmail.com>
2025-01-09 15:12:38 +0800

    freq dist and box and whiskey diagram for the word count and sentiment scores, also refactor to seperate the visualize part out

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ def analyze_wordcount_stat(analysed_poems):
     freq_dist = {count: freq for count, freq in zip(unique_counts[0], unique_counts[1])}
 
     # Plot histogram of word counts
+    """
     plt.figure(figsize=(8, 6))
     plt.hist(word_counts, bins=5, color='skyblue', edgecolor='black', alpha=0.7)
     plt.title('Word Count Distribution', fontsize=16)
@@ -45,6 +46,7 @@ def analyze_wordcount_stat(analysed_poems):
     plt.ylabel('Frequency', fontsize=14)
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.show()
+    """
     
     # Calculate statistics
     stats = {
@@ -53,11 +55,58 @@ def analyze_wordcount_stat(analysed_poems):
         'Standard Deviation': round(np.std(word_counts), 2),
         'Min': np.min(word_counts),
         'Max': np.max(word_counts),
-        'freq_dist': freq_dist
+        'freq_dist': freq_dist,
+        'word_counts': word_counts
     }
 
-    # Plot Box diagram of word counts
+    
     return stats
+
+# function to visualize the word count stat: freq dist and box diagrams
+def visualize_wordcount_stat(wc_stats):
+    # plot freq dist for wc
+    word_counts = wc_stats['word_counts']
+
+    plt.figure(figsize=(8, 6))
+    plt.hist(word_counts, bins=5, color='skyblue', edgecolor='black', alpha=0.7)
+    plt.title('Word Count Distribution', fontsize=16)
+    plt.xlabel('Word Count', fontsize=14)
+    plt.ylabel('Frequency', fontsize=14)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
+
+    # box and whisker plot
+    plt.figure(figsize=(8, 6))
+    plt.boxplot(word_counts, vert=False) #don't know why the whiskey line don't show
+    
+    plt.title('Word Count Distribution (box and whiskey)', fontsize=16)
+    plt.xlabel('Word Count', fontsize=14)
+    plt.yticks([])
+    plt.grid(axis='x', linestyle='--', alpha=0.7)
+    plt.show()
+
+def visualize_sentiment_stat(sentiment_stats):
+    # plot freq dist for sentiment
+    sentiments = sentiment_stats['sentiments']
+    plt.figure(figsize=(8, 6))
+    plt.hist(sentiments, bins=5, color='skyblue', edgecolor='black', alpha=0.7)
+    plt.title('Sentiment Score Distribution', fontsize=16)
+    plt.xlabel('Sentiment Score', fontsize=14)
+    plt.ylabel('Frequency', fontsize=14)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    plt.show()
+
+    # box and whisker plot
+    plt.figure(figsize=(8, 6))
+    plt.boxplot(sentiments, vert=False) #don't know why the whiskey line don't show
+    
+    plt.title('Sentiment Score Distribution (box and whiskey)', fontsize=16)
+    plt.xlabel('Sentiment score', fontsize=14)
+    plt.yticks([])
+    plt.grid(axis='x', linestyle='--', alpha=0.7)
+    plt.show()
+
+
 
 # func to analyse the sentiment of poems
 def analyze_sentiment_stat(analyszed_poems):
@@ -67,7 +116,8 @@ def analyze_sentiment_stat(analyszed_poems):
     overall_stats = {
         'average_sentiment': np.mean(sentiments),
         'median_sentiment': np.median(sentiments),
-        'std_sentiment': np.std(sentiments)
+        'std_sentiment': np.std(sentiments),
+        'sentiments': sentiments
     }
     return overall_stats
 
@@ -132,10 +182,12 @@ for filename in os.listdir(data_folder):
 # analyze
 analyses = [analyze_poem(poem) for poem in poems]
 wordcount_stat = analyze_wordcount_stat(analyses)
-generate_word_cloud(poems)
 sentiment_result = analyze_sentiment_stat(analyses)
 
 # Display results (TODO visualize)
+# generate_word_cloud(poems)
+visualize_wordcount_stat(wordcount_stat)
+visualize_sentiment_stat(sentiment_result)
 print("Statistical Analysis of Poem Word Counts:")
 print(wordcount_stat)
 print(sentiment_result)
