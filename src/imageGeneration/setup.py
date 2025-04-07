c349985d73f7b4f0e09f77a0d3d15d07f5420797
sam1037 <samngai2522@gmail.com>
2025-04-07 11:10:07 +0800

    now git won't ignore gallery output HTML files

--- src/imageGeneration/setup.py
+++ src/imageGeneration/setup.py
@@ -15,7 +15,7 @@ sys.path.append(parent_dir)
 # Now you can import the file
 import dataLoader
 
-async def generateForOneIssue(issueNum, textModel, imgMode):
+async def generateForOneIssue(issueNum, textModel, imgMode, skip_poems = []):
     print(f"Generating images for issue {issueNum} using {textModel} and {imgMode}.")
 
     # load the poems for issue 1 and generate the images
@@ -28,6 +28,10 @@ async def generateForOneIssue(issueNum, textModel, imgMode):
         if os.path.exists(f"generatedImages/actual/issueNumber{issueNum}/{poemName}_{textModel}_{imgMode}.png"):
             print(f"Image for {poemName} in issue {issueNum} already exist.")
             continue
+        # check if the poem is in the skip list
+        if poemName in skip_poems:
+            print(f"Skipping {poemName} in issue {issueNum}.")
+            continue
         
         # Generate images for the poem using different models
         print(f"Generating image for {poemName} in issue {issueNum}.")
@@ -38,10 +42,22 @@ async def main():
     gpt4o = "gpt4_o"
     flux = "flux-pro-1.1"
 
+    skip_poems = ["拿槍的孩子"]
+
+    poem = dataLoader.loadOnePoem("我病了還是這個社會病了", 34)
+    time.sleep(10)
+    await generateAndSaveImg(poem, gpt4o, flux)
+
+    """
     # load the poems for a particular issue and generate the images
-    issues = [10,11,12]
-    for issueNum in issues:
-        await generateForOneIssue(issueNum, gpt4o, flux)
+    
+    for issueNum in range(1,36):
+        await generateForOneIssue(issueNum, gpt4o, flux, skip_poems)
+
+    # for good measure
+    for issueNum in range(1,36):
+        await generateForOneIssue(issueNum, gpt4o, flux, skip_poems)
+    """
 
 # Run the async main function
 if __name__ == "__main__":
